9f377d608c3cd848f54f8a7dd8e6dfd0e6ac382b
MajorPayne36 <53790991+MajorPayne36@users.noreply.github.com>
2020-03-28 23:37:02 +0300

    HEAD
    .m
    
    Co-Authored-By: Андраник Вачаганович Григорян <majorpayne36@users.noreply.github.com>

---
+++ Project_K/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_K.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            return View();
+        }
+
+        public ActionResult Vvod()
+        {
+
+            return View();
+        }
+        public ActionResult Test()
+        {
+
+            return View();
+        }
+        public ActionResult Teoria()
+        {
+
+            return View();
+        }
+        public ActionResult Reg()
+        {
+
+            return View();
+        }
+    }
+}
